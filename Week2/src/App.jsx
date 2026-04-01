9d1728685582260b306a0035f55fbe96161a2ac6
hyKim <rlagjsdud3@hansung.ac.kr>
2026-04-02 00:14:34 +0900

    feat: About 섹션 컴포넌트 및 SCSS 구현

--- Week2/src/App.jsx
+++ Week2/src/App.jsx
@@ -1,12 +1,16 @@
 import './styles/App.scss'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import About from './components/About'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <main className="content-wrap">
+        <About />
+      </main>
     </>
   )
 }
